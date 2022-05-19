9f88d28b7e643c555386c2428b135ce1a58db2e9
opm <aneessabawi@gmail.com>
2022-05-18 22:49:14 -0400

    Begin image combiner

---
+++ combiner/src/args.rs
@@ -0,0 +1,22 @@
+use std::env::args;
+
+#[derive(Debug)]
+pub struct Args {
+    pub image_1: String,
+    pub image_2: String,
+    pub output: String,
+}
+
+pub fn get_nth_arg(n: usize) -> String {
+    args().nth(n).unwrap()
+}
+
+impl Args {
+    pub fn new() -> Self {
+        Args {
+            image_1: get_nth_arg(1),
+            image_2: get_nth_arg(2),
+            output: get_nth_arg(3),
+        }
+    }
+}
--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+use args::Args;
+use std::{
+    io::BufReader,
+    fs::File
+};
+use image::{
+    io::Reader,
+    DynamicImage,
+    ImageFormat
+};
+
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+
+    (image, image_format)
 }
